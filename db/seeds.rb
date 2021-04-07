3f644eb4a2fd6398caf3a85021a5afb2bfb05462
Fabricio Butron <fa.butron.rodriguez@gmail.com>
2021-04-07 10:27:28 -0400

    Created index view, function, route and seed for Character and User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.find_or_initialize_by(email: 'fa@gmail.com')
+user.password = '123456'
+user.password_confirmation = '123456'
+
+
+character = Character.find_or_initialize_by(name: 'Brad Pitt', age: '35', history: 'Best male actor on 90s')
+user.save
+character.save
